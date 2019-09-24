8e40c63a4065c526c36498d25876a62330d40caa
André Almeida <dealmeida_andre@hotmail.com>
2019-09-24 21:27:17 +0100

    Interaction 1 and 2

--- starter-code/src/App.jsx
+++ starter-code/src/App.jsx
@@ -1,21 +1,25 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from 'react';
+import foods from './foods.json';
+import './App.css';
+import FoodBox from './components/foodbox.jsx';
+import Form from './components/form.jsx';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
 
+  addFoodForm() {
+    this.setState({
+      addFoodForm: !this.state.addFoodForm,
+    });
+  }
+
+
+ render() {
+   return (
+     <div className="App">
+       <FoodBox foods={foods} />
+     </div>
+   );
+ }
+}
 export default App;
